b78a4663610cfcdda5689e009c922dc2b075a74d
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-03-01 22:26:17 -0300

    update whitelist sites

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ import checkLoginRoutes from './routes/checkLoginRoutes';
 
 const whiteList = [
   process.env.WHITE_LIST_MAIN_URL,
-  '*',
+  'http://localhost:5173/',
 ];
 
 const corsOptions = {
