358ed355bb50043707e7fe07426730cef52cce92
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-03-04 15:40:45 -0300

    fix: status 400 when happens errors

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -7,7 +7,7 @@ class LoginController {
       const login = new Login(req.body);
       await login.login();
       if (login.errors.length > 0) {
-        return res.status(200).json({ errorsMsg: login.errors });
+        return res.status(400).json({ errorsMsg: login.errors });
       }
       const { id, email } = login.user;
       const token = jwt.sign({ id, email }, process.env.TOKEN_SECRET, {
